e510139be3fde4bcc72b6981e736e2ebb293ef97
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-06-08 17:55:42 +0900

    프로젝트 생성

---
+++ OneulPrj/src/main/java/com/newlecture/web/OneulPrjApplication.java
@@ -0,0 +1,13 @@
+package com.newlecture.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneulPrjApplication.class, args);
+	}
+
+}
