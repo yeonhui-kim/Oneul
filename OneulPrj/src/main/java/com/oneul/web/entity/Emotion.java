4d3c808a372adb676876273837880644c7b17469
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-06-09 18:01:48 +0900

    entitiy (감정, 감사일기)

---
+++ OneulPrj/src/main/java/com/oneul/web/entity/Emotion.java
@@ -0,0 +1,28 @@
+package com.oneul.web.entity;
+
+public class Emotion {
+	
+	private Integer id;
+	private String name;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Emotion [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
