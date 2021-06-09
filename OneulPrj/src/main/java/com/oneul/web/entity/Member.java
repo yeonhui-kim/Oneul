8ee566d8cee0cb83be3ac9b21e0089d18697c5f8
hmountainn <ghkdwltks123@naver.com>
2021-06-09 19:34:31 +0900

    Memeber( class, controller, service, dao ) 생성

---
+++ OneulPrj/src/main/java/com/oneul/web/entity/Member.java
@@ -0,0 +1,82 @@
+package com.oneul.web.entity;
+
+import java.sql.Date;
+
+public class Member {
+	private Integer id;
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+	private Date birthday;
+	private String image;
+	private String introduction;
+	
+	public Member(Integer id, String userId, String password, String name, String email, Date birthday, String image,
+			String introduction) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.birthday = birthday;
+		this.image = image;
+		this.introduction = introduction;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", userId=" + userId + ", password=" + password + ", name=" + name + ", email="
+				+ email + ", birthday=" + birthday + ", image=" + image + ", introduction=" + introduction + "]";
+	}
+	
+	
+}
---
+++ OneulPrj/src/main/java/com/oneul/web/service/MemberServiceimp.java
@@ -0,0 +1,39 @@
+package com.oneul.web.service;
+
+import java.util.List;
+
+import com.oneul.web.entity.Member;
+
+public class MemberServiceimp implements MemberService {
+
+	@Override
+	public Member get(int id) {
+
+		return null;
+	}
+
+	@Override
+	public List<Member> getList() {
+
+		return null;
+	}
+
+	@Override
+	public int insert(Member member) {
+
+		return 0;
+	}
+
+	@Override
+	public int update(Member member) {
+
+		return 0;
+	}
+
+	@Override
+	public int delete(int id) {
+
+		return 0;
+	}
+
+}
