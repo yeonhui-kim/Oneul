95e2c9cc7758b0b9a2bb24de604cbec6b9b5f250
haileykim2014 <haileykim2014@gmail.com>
2021-06-10 13:12:36 +0900

    test.html

--- OneulPrj/src/main/java/com/oneul/web/dao/mybatis/MyBatisFutureDiaryDao.java
+++ OneulPrj/src/main/java/com/oneul/web/dao/mybatis/MyBatisFutureDiaryDao.java
@@ -1,56 +1,56 @@
-package com.oneul.web.dao.mybatis;
-
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-
-import com.oneul.web.dao.FutureDiaryDao;
-import com.oneul.web.entity.FutureDiary;
-
-public class MyBatisFutureDiaryDao implements FutureDiaryDao {
-
-	private SqlSession sqlSession;
-	private FutureDiaryDao mapper;
-	
-	public MyBatisFutureDiaryDao(SqlSession sqlSession) {
-		this.sqlSession = sqlSession;
-		mapper = sqlSession.getMapper(FutureDiaryDao.class);
-	}
-	
-	@Override
-	public FutureDiary get(int id) {
-		return mapper.get(id);
-		
-	}
-
-	@Override
-	public List<FutureDiary> getList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<FutureDiary> getList(int offset, int size) {
-		// TODO Auto-generated method stub
-		return mapper.getList(offset, size);
-	}
-
-	@Override
-	public int insert(FutureDiary futureDiary) {
-		// TODO Auto-generated method stub
-		return mapper.insert(futureDiary);
-	}
-
-	@Override
-	public int update(FutureDiary futureDiary) {
-		// TODO Auto-generated method stub
-		return mapper.update(futureDiary);
-	}
-
-	@Override
-	public int delete(int id) {
-		// TODO Auto-generated method stub
-		return mapper.delete(id);
-	}
-
-}
+//package com.oneul.web.dao.mybatis;
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.session.SqlSession;
+//
+//import com.oneul.web.dao.FutureDiaryDao;
+//import com.oneul.web.entity.FutureDiary;
+//
+//public class MyBatisFutureDiaryDao implements FutureDiaryDao {
+//
+//	private SqlSession sqlSession;
+//	private FutureDiaryDao mapper;
+//	
+//	public MyBatisFutureDiaryDao(SqlSession sqlSession) {
+//		this.sqlSession = sqlSession;
+//		mapper = sqlSession.getMapper(FutureDiaryDao.class);
+//	}
+//	
+//	@Override
+//	public FutureDiary get(int id) {
+//		return mapper.get(id);
+//		
+//	}
+//
+//	@Override
+//	public List<FutureDiary> getList() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<FutureDiary> getList(int offset, int size) {
+//		// TODO Auto-generated method stub
+//		return mapper.getList(offset, size);
+//	}
+//
+//	@Override
+//	public int insert(FutureDiary futureDiary) {
+//		// TODO Auto-generated method stub
+//		return mapper.insert(futureDiary);
+//	}
+//
+//	@Override
+//	public int update(FutureDiary futureDiary) {
+//		// TODO Auto-generated method stub
+//		return mapper.update(futureDiary);
+//	}
+//
+//	@Override
+//	public int delete(int id) {
+//		// TODO Auto-generated method stub
+//		return mapper.delete(id);
+//	}
+//
+//}
--- OneulPrj/src/main/java/com/oneul/web/dao/mybatis/MyBatisGratitudeDiaryDao.java
+++ OneulPrj/src/main/java/com/oneul/web/dao/mybatis/MyBatisGratitudeDiaryDao.java
@@ -1,51 +1,51 @@
-package com.oneul.web.dao.mybatis;
-
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-
-import com.oneul.web.dao.GratitudeDiaryDao;
-import com.oneul.web.entity.GratitudeDiary;
-
-public class MyBatisGratitudeDiaryDao implements GratitudeDiaryDao {
-
-	private SqlSession SqlSession;
-	private GratitudeDiaryDao mapper;
-	
-	public MyBatisGratitudeDiaryDao(SqlSession sqlSession) {
-		this.SqlSession = sqlSession;
-		mapper = SqlSession.getMapper(GratitudeDiaryDao.class);
-	}
-	
-	@Override
-	public GratitudeDiary get(int id) {
-		return mapper.get(id);
-	}
-
-	@Override
-	public List<GratitudeDiary> getList() {
-		return getList();
-	}
-
-	@Override
-	public List<GratitudeDiary> getList(int offset, int size) {
-		return getList(offset, size);
-	}
-
-	@Override
-	public int insert(GratitudeDiary gratitudeDiary) {
-		return mapper.insert(gratitudeDiary);
-	}
-
-	@Override
-	public int update(GratitudeDiary gratitudeDiary) {
-		return mapper.update(gratitudeDiary);
-	}
-
-	@Override
-	public int delete(int id) {
-		return mapper.delete(id);
-	}
-
-	
-}
+//package com.oneul.web.dao.mybatis;
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.session.SqlSession;
+//
+//import com.oneul.web.dao.GratitudeDiaryDao;
+//import com.oneul.web.entity.GratitudeDiary;
+//
+//public class MyBatisGratitudeDiaryDao implements GratitudeDiaryDao {
+//
+//	private SqlSession SqlSession;
+//	private GratitudeDiaryDao mapper;
+//	
+//	public MyBatisGratitudeDiaryDao(SqlSession sqlSession) {
+//		this.SqlSession = sqlSession;
+//		mapper = SqlSession.getMapper(GratitudeDiaryDao.class);
+//	}
+//	
+//	@Override
+//	public GratitudeDiary get(int id) {
+//		return mapper.get(id);
+//	}
+//
+//	@Override
+//	public List<GratitudeDiary> getList() {
+//		return getList();
+//	}
+//
+//	@Override
+//	public List<GratitudeDiary> getList(int offset, int size) {
+//		return getList(offset, size);
+//	}
+//
+//	@Override
+//	public int insert(GratitudeDiary gratitudeDiary) {
+//		return mapper.insert(gratitudeDiary);
+//	}
+//
+//	@Override
+//	public int update(GratitudeDiary gratitudeDiary) {
+//		return mapper.update(gratitudeDiary);
+//	}
+//
+//	@Override
+//	public int delete(int id) {
+//		return mapper.delete(id);
+//	}
+//
+//	
+//}
