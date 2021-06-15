97a88806076e52648333b792f9c47d3bfccb85cb
hmountainn <ghkdwltks123@naver.com>
2021-06-15 13:09:32 +0900

    파일 업로드 구현

--- OneulPrj/src/main/java/com/oneul/web/controller/MemberController.java
+++ OneulPrj/src/main/java/com/oneul/web/controller/MemberController.java
@@ -1,11 +1,17 @@
 package com.oneul.web.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Date;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.oneul.web.entity.Member;
 import com.oneul.web.service.MemberService;
@@ -53,6 +59,40 @@ public class MemberController {
 		//헬로
 	}
 	
+	
+	@RequestMapping("uploadtest")
+	public String upload() {
+		return "member/uploadtest";
+	}
+	
+	@PostMapping("upload")
+	public String upload(MultipartFile file, HttpServletRequest request) {
+		
+		String fileName = file.getOriginalFilename();//파일이름
+		
+		ServletContext application = request.getServletContext();
+		String path = "/upload";
+		String realPath = application.getRealPath(path);
+		
+		File pathFile = new File(realPath);
+		if(!pathFile.exists()) //upload할 경로 존재x하면
+			pathFile.mkdirs(); //경로 생성
+		
+		String filePath = realPath + File.separator + fileName;
+		
+		File saveFile = new File(filePath); //저장할 파일
+		
+		try {
+			file.transferTo(saveFile);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "member/uploadtest";
+	}
+
 }
 
 
