9645afe82d291f52f5bc76fe010ce744d98342d0
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-06-21 18:52:51 +0900

    감정선택 모달창, 감정아이콘클래스 생성

--- OneulPrj/src/main/resources/static/js/modal/modal.js
+++ OneulPrj/src/main/resources/static/js/modal/modal.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", function (event) {
 	// Get the modal
-	var modal = document.getElementById("modal");
+	let modal = document.getElementById("modal");
 	
 	// Get the button that opens the modal
-	var openBtn = document.getElementById("open-button");
+	let openBtn = document.getElementById("open-button");
 	
 	// Get the <span> element that closes the modal
-	var closePage = document.getElementsByClassName("close")[0];
+	let closePage = document.getElementsByClassName("close")[0];
 	
+	let emotion = document.querySelector("#emotion");
+	console.log(emotion);
 	
 	// When the user clicks on the button, open the modal 
 	openBtn.onclick = function() {
@@ -18,6 +20,33 @@ window.addEventListener("load", function (event) {
 	// When the user clicks on <span> (x), close the modal
 	closePage.onclick = function() {
 		modal.style.display = "none";
+		emotion.style.display = "inline-block";
+		
+		let checkedEmotion = document.querySelector('input[name="emotionId"]:checked').value;
+		if(checkedEmotion != null){
+			//emotion.innerText = checkedEmotion;
+			
+			
+			if(checkedEmotion == 1){
+				emotion.className = 'happy';
+			}else if(checkedEmotion == 2){
+				emotion.className = 'proud';
+			}else if(checkedEmotion == 3){
+				emotion.className = 'flutter';
+			}else if(checkedEmotion == 4){
+				emotion.className = 'calm';
+			}else if(checkedEmotion == 5){
+				emotion.className = 'sensitive';
+			}else if(checkedEmotion == 6){
+				emotion.className = 'unstable';
+			}else if(checkedEmotion == 7){
+				emotion.className = 'tired';
+			}else if(checkedEmotion == 8){
+				emotion.className = 'sad';
+			}else{
+				emotion.className = 'upset';
+			}
+		}
 	}
 	
 	
