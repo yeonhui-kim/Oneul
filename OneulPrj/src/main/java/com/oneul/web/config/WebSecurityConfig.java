2acadb546453a6619bfb12dfccc54d044f395934
hongssi9 <79443935+hongssi9@users.noreply.github.com>
2021-06-26 23:01:57 +0900

    게시글,댓글(수정,삭제 권한 )

--- OneulPrj/src/main/java/com/oneul/web/config/WebSecurityConfig.java
+++ OneulPrj/src/main/java/com/oneul/web/config/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.antMatchers("/member/mypagetest").hasAnyRole("MEMBER")
 				.antMatchers("/member/edit").hasAnyRole("MEMBER")
 				.antMatchers("/member/mypage").hasAnyRole("MEMBER")
+				.antMatchers("/feed/oneline/reg").hasAnyRole("MEMBER")
 				.and() // 경로 설정 끝냄
 			.formLogin() //권한x일때 로그인페이지로
 				.loginPage("/member/login")
--- OneulPrj/src/main/java/com/oneul/web/controller/OnelineController.java
+++ OneulPrj/src/main/java/com/oneul/web/controller/OnelineController.java
@@ -34,24 +34,62 @@ public class OnelineController {
    private MemberService mservice;
 
    @RequestMapping("list")
-   public String list(Model model) {
-      List<Oneline> list = service.getList(); 
-		/* list.get(0).getContent() */
+   public String list(Model model, HttpServletRequest request) {
+      List<Oneline> list = service.getList();
+      /* list.get(0).getContent() */
       model.addAttribute("list", list);
-
+      
+      //-----------------------------------------------------
+      HttpSession session = request.getSession(true);
+      String username = (String) session.getAttribute("username");    
+      Member member2 = mservice.get(username);
+     
+      Member member = new Member();
+      member.setUserId(username);
+      model.addAttribute("member", member2);
       
       return "feed/oneline/list";
    }
 
-   @RequestMapping("detail")
-   public String detail(Model model, int id) {
+   @GetMapping("detail")
+   public String detail(Model model, int id, HttpServletRequest request) {
       Oneline oneline = service.get(id);
       model.addAttribute("detail", oneline);
 
       List<OnelineComment> clist = cservice.getViewList(id);
       model.addAttribute("clist", clist);
+      
+      HttpSession session = request.getSession(true);
+      String username = (String) session.getAttribute("username");    
+      Member member2 = mservice.get(username);
+     
+      Member member = new Member();
+      member.setUserId(username);
+      model.addAttribute("member", member2);
 
       return "feed/oneline/detail";
+   }
+
+   // ---------------detail에서 댓글 입력---------------------------
+   @PostMapping("CommentReg") // 댓글등록 1.멤버아이디,content,onelineId(게시글아이디),writerId로그인사용자아이디,image
+   public String commentReg(HttpServletRequest request, String comment ,int id) {
+      // ------로그인 유저네임-------
+      HttpSession session = request.getSession(true);
+      String username = (String) session.getAttribute("username");
+      Member member2 = mservice.get(username); 
+      Integer memberId = member2.getId(); //로그인한 멤버 아이디
+      String writerId = member2.getUserId(); // 로그인 유저아이디
+      String image = member2.getImage(); //로그인 이미지
+      
+      OnelineComment onelineComment = new OnelineComment();
+      onelineComment.setContent(comment);
+      onelineComment.setMemberId(memberId);
+      onelineComment.setWriterId(writerId);
+      onelineComment.setImage(image);
+      onelineComment.setOnelineId(id); //현재 게시글 아이디 넣어주기....                           
+      cservice.insert(onelineComment);
+
+      return "redirect:detail?id=" + id;
    }
 
    @GetMapping("reg")
@@ -60,25 +98,24 @@ public class OnelineController {
       return "feed/oneline/reg";
    }
 
-   @PostMapping("reg")
-   public String reg(String content,String writerId, HttpServletRequest request, Model model) {
+   @PostMapping("reg") // list reg
+   public String reg(String content,HttpServletRequest request, Model model) {
 
       // ------로그인 유저네임-------
       HttpSession session = request.getSession(true);
-      String username = (String) session.getAttribute("username");//1.세션에 가져온 유저네임을 문자열로 만들었다
+      String username = (String) session.getAttribute("username");// 1.세션에 가져온 유저네임을 문자열로 만들었다
 
-      Member member2 = mservice.get(username); //2.서비스객체에 .get(세션에가져온이름)을 넣었다
-      String name = member2.getName();
-      Integer memberId = member2.getId(); //로그인세션 memberId 
-      String writerId2 = member2.getUserId(); //로그인 아이디
+      Member member2 = mservice.get(username); // 2.서비스객체에 .get(세션에가져온이름)을 넣었다
+      String name = member2.getName(); //로그인 이름
+      Integer memberId = member2.getId(); // 로그인세션 memberId
+      String writerId2 = member2.getUserId(); // 로그인 아이디
       String image = member2.getImage();
-      
-      Member member = new Member();
-      member.setUserId(username);
-      member.setName(name);
-      member.setImage(image);
-      model.addAttribute("member", member2);
-      
+
+//      Member member = new Member();
+//      member.setUserId(username);
+//      member.setName(name);
+//      member.setImage(image);
+//      model.addAttribute("member", member2);
 
       Oneline oneline = new Oneline();
       oneline.setContent(content);
@@ -93,9 +130,16 @@ public class OnelineController {
    }
 
    @RequestMapping("del")
-   public String reg(int id) {
+   public String del(int id) {
 
       service.delete(id);
+      return "redirect:list";
+   }
+
+   @RequestMapping("cdel")
+   public String cdel(int id) {
+
+      cservice.delete(id);
       return "redirect:list";
    }
 
