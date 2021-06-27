19993d6da3976ade418efa7cab2ac1d95ca8afe1
hongssi9 <79443935+hongssi9@users.noreply.github.com>
2021-06-27 18:33:25 +0900

    로그인안하면 list못봄...

--- OneulPrj/src/main/java/com/oneul/web/config/WebSecurityConfig.java
+++ OneulPrj/src/main/java/com/oneul/web/config/WebSecurityConfig.java
@@ -26,6 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.antMatchers("/diary/freediary/reg").hasAnyRole("MEMBER")
 				.antMatchers("/diary/freediary/list").hasAnyRole("MEMBER")
 				.antMatchers("/feed/oneline/reg").hasAnyRole("MEMBER")
+				.antMatchers("/feed/oneline/list").hasAnyRole("MEMBER")
 				
 				.and() // 경로 설정 끝냄
 			.formLogin() //권한x일때 로그인페이지로
