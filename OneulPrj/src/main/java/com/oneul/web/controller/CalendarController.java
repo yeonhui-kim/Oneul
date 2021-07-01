ca26fe9c16e62ed64e7321a2691746a404c9ebb3
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-07-01 11:47:54 +0900

    자유일기 수정시에 regDate 업데이트

--- OneulPrj/src/main/java/com/oneul/web/controller/CalendarController.java
+++ OneulPrj/src/main/java/com/oneul/web/controller/CalendarController.java
@@ -31,29 +31,6 @@ public class CalendarController {
 	@RequestMapping("list")
 	public String list(HttpServletRequest request, @RequestParam(name="currentMonth", required = false) String currentMonth ,Model model) {
 		
-//		Map<String, String> event1 = new HashMap<String, String>();
-//		event1.put("title", "일정1");
-//		event1.put("start", "2021-06-22");
-//		event1.put("id", "event1");
-//		
-//		Map<String, String> event2 = new HashMap<String, String>();
-//		event2.put("title", "일정2");
-//		event2.put("start", "2021-06-23");
-//		event2.put("id", "event3");
-//		
-//		Map<String, String> event3 = new HashMap<String, String>();
-//		event3.put("title", "일정3");
-//		event3.put("start", "2021-06-24");
-//		event3.put("id", "event3");
-//		
-//		List<Map<String, String>> jsonList = new ArrayList<Map<String, String>>();
-//		
-//		jsonList.add(event1);
-//		jsonList.add(event2);
-//		jsonList.add(event3);
-//		
-//		
-		
 		HttpSession session = request.getSession(true);//세션에 유저네임을 넣어놨다->해당유저네임을꺼내기
 		String username = (String) session.getAttribute("username");
 		Member member = memberSerivce.get(username);
@@ -72,7 +49,6 @@ public class CalendarController {
 			
 		}
 
-		
 		model.addAttribute("jsonString", json);
 		
 		return "calendar/calendar";
