10f431c1ec9b73438e5ea409a5198b73d29ca9f8
hmountainn <ghkdwltks123@naver.com>
2021-07-06 18:29:55 +0900

    로그인 필요페이지 수정

--- OneulPrj/src/main/java/com/oneul/web/config/WebSecurityConfig.java
+++ OneulPrj/src/main/java/com/oneul/web/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/member/**").hasAnyRole("MEMBER")
+				.antMatchers("/member/edit").hasAnyRole("MEMBER")
 				.antMatchers("/diary/**").hasAnyRole("MEMBER")
 				.antMatchers("/feed/**").hasAnyRole("MEMBER")
 				.antMatchers("/calendar/**").hasAnyRole("MEMBER")
