e32eeea6862dce335a3152f519f2957e85eb3301
ohhyeon <hyunong2@gmail.com>
2021-07-11 03:37:08 +0900

    profile popup

---
+++ OneulPrj/src/main/java/com/oneul/web/api/controller/ProfileController.java
@@ -0,0 +1,25 @@
+package com.oneul.web.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oneul.web.entity.Member;
+import com.oneul.web.service.MemberServiceimp;
+
+@RestController("ApiProfileController")
+public class ProfileController {
+	@Autowired
+	private MemberServiceimp service;
+	
+	
+	@CrossOrigin("http://127.0.0.1:5500/")
+	@RequestMapping("/api/profile/{username}")
+	public Member profile(@PathVariable(name ="username") String userName) {
+		
+		
+		return service.get(userName);
+	}
+}
